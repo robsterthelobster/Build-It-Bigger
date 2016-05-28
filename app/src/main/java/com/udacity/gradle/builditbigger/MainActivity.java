c4e05fcc498adb82ad5e0fff8be3e59c8722ab19
Robin Chen <robin3chen@gmail.com>
2016-05-27 17:50:11 -0700

    flavors and ad-removal for paid

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -9,6 +9,7 @@ import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.udacity.gradle.builditbigger.gce.EndpointsAsyncTask;
@@ -20,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView) findViewById(R.id.package_name)).setText(getPackageName());
     }
 
 
